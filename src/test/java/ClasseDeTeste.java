395726cb117734d3ee35770b95314f6f5230f8e1
Cindia <cindiams@gmail.com>
2021-10-25 16:16:15 -0300

    Cenário de Teste Listar IDs das Reservas

--- src/test/java/ClasseDeTeste.java
+++ src/test/java/ClasseDeTeste.java
@@ -2,6 +2,7 @@ import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class ClasseDeTeste {
 
@@ -16,5 +17,18 @@ public class ClasseDeTeste {
         responsePing
                 .then()
                 .statusCode(201);
+    }
+    @Test
+    public void validaListagemDeIdsDASReservas(){
+        Response responseListagemIdsReservas =
+                given()
+                        .when()
+                        .get("https://treinamento-api.herokuapp.com/booking");
+
+        responseListagemIdsReservas
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0));
+
     }
 }
